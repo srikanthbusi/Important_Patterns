9fb27690d219817c6176da30119b8971879fa4ae
Srikanth Busi <143828965+srikanthbusi@users.noreply.github.com>
2025-02-16 17:20:20 +0530

    Create 1.Patterns.cpp

---
+++ 1.Patterns.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+using namespace std;
+
+void pattern(int n){
+    for(int i = 0;i<n;i++){
+        for(int j= 0; j<n; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+int main() {
+    // Write C++ code here
+    int n;
+    cin >> n;
+    pattern(n);
+    
+
+    return 0;
+}
